46b11705c2fa1bde14dd1c75341393091ed0a296
ben <bmfboom9@gmail.com>
2025-08-01 11:30:44 -0600

    add controller support

--- Loop_GMTKJAM2025/Assets/_Scripts/WheelController.cs
+++ Loop_GMTKJAM2025/Assets/_Scripts/WheelController.cs
@@ -1,22 +1,74 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(PlayerInput))]
 public class WheelController : MonoBehaviour
 {
+    
+
     [SerializeField] private Transform weaponWheel;
     [SerializeField] private float rotation;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    PlayerInput input;
+
+    PlayerControls playerControls;
+
+    bool gamepad = false;
+    [SerializeField] float controllerDeadzone = 0.1f;
+
+    private void Start()
     {
-        FollowCursor();
+        input = GetComponent<PlayerInput>();
+        playerControls = new PlayerControls();
     }
 
-    public void FollowCursor()
+    public void FollowCursor(Vector2 direction)
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePos - transform.position;
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rotation = rotZ - 90;
         weaponWheel.rotation = Quaternion.Euler(0, 0, rotation);
+    }
+
+    private void Update()
+    {
+        // no gamepad, always point at mouse
+        if (!gamepad) { FollowCursor(GetVectorToMousePos()); }
+       
+    }
+
+    void OnControlsChanged(PlayerInput input)
+    {
+        gamepad = input.currentControlScheme.Equals("Gamepad");
+    }
+
+    void OnLook(InputValue input)
+    {
+
+        Vector2 aimInput = input.Get<Vector2>();
+
+        Vector2 aimDirection = Vector2.zero;
+
+        if (gamepad)
+        {
+
+            if (Mathf.Abs(aimInput.x) > controllerDeadzone || Mathf.Abs(aimInput.y) > controllerDeadzone)
+            {
+                aimDirection = aimInput;
+            }
+        }
+        else
+        {
+            aimDirection = GetVectorToMousePos();
+        }
+
+        FollowCursor(aimDirection);
+    }
+
+    private Vector2 GetVectorToMousePos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        return direction;
     }
 }
